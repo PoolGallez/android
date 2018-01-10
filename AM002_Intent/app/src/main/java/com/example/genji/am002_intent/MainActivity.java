34a14936882afade1c715fa5bafc1e47a1b3413f
Andrea Morettin <you@example.com>
2018-01-10 13:00:00 +0100

    AM002 new version

--- AM002_Intent/app/src/main/java/com/example/genji/am002_intent/MainActivity.java
+++ AM002_Intent/app/src/main/java/com/example/genji/am002_intent/MainActivity.java
@@ -3,9 +3,8 @@ package com.example.genji.am002_intent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,21 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, ActivityTwo.class);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener((view)-> {
 
+                Intent i = new Intent(this, ActivityTwo.class);
+
+                // put extra
                 i.putExtra("str1", "Message Example");
                 i.putExtra("n1", 25);
 
+                // put extras
                 Bundle extras = new Bundle();
                 extras.putString("str2", "This is another string");
                 extras.putInt("n2", 666);
                 i.putExtras(extras);
 
                 startActivityForResult(i, ACTIVITY_TWO);
-            }
         });
     }
 
@@ -40,9 +40,8 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == ACTIVITY_TWO) {
             //---if the result is OK---
             if (resultCode == RESULT_OK) {
-                // data passed in using getStringExtra() via Intent data
-                Toast.makeText(this, data.getStringExtra("isOK"),
-                        Toast.LENGTH_SHORT).show();
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(getResources().getText(R.string.main_activity_returned));
             }
         }
     }

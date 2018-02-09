11506780073902d1945934100a47ed947279bb74
Andrea Morettin <andrea.morettin@itiszuccante.gov.it>
2018-02-09 09:55:47 +0100

    AS1718 updated

--- AM013_ListView/app/src/main/java/com/example/genji/am013_listview/MainActivity.java
+++ AM013_ListView/app/src/main/java/com/example/genji/am013_listview/MainActivity.java
@@ -1,57 +1,81 @@
 package com.example.genji.am013_listview;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private ArrayAdapter adapter;
+    private List products;
 
-    private String[] products = {
-            "gioppini",
-            "jambonetti",
-            "patatine sfizione",
-            "tarallini",
-            "gallette",
-            "frollini plus",
-            "cioccolini",
-            "secchini",
-            "grissinini",
-            "patasplash",
-            "majopatas",
-            "crocchette al sesamo",
-            "crocchette alla pancetta",
-            "biscotti al miglio e avena"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mListView = (ListView) findViewById(R.id.my_list_view);
+        initializeProducts();
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, products);
-        /* // lstView.setChoiceMode(ListView.CHOICE_MODE_NONE);
+        mListView = findViewById(R.id.my_list_view);
+        adapter = new ArrayAdapter(this, android.R.layout.test_list_item, products);
+        /*
+        mListView.setChoiceMode(ListView.CHOICE_MODE_NONE);
         mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        // lstView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_checked, products); */
+                android.R.layout.simple_list_item_checked, products);
+        */
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,"You selected : " + products[position], Toast.LENGTH_SHORT).show();
-                /* CheckedTextView item = (CheckedTextView) view;
-                Toast.makeText(MainActivity.this, products[position] + " checked : " +
-                        item.isChecked(), Toast.LENGTH_SHORT).show(); */
+                Toast.makeText(MainActivity.this,"Remove : " + products.get(position), Toast.LENGTH_SHORT).show();
+                products.remove(position);
+                adapter.notifyDataSetChanged();
             }
         });
     }
+
+    public void populate(View view){
+        Toast.makeText(this, "RESET", Toast.LENGTH_LONG).show();
+        adapter.clear();
+        initializeProducts();
+        adapter.addAll(products);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initializeProducts(){
+        products = new ArrayList<String>(Arrays.asList(
+                "gioppini",
+                "jambonetti",
+                "patatine sfizione",
+                "tarallini",
+                "gallette",
+                "frollini plus",
+                "cioccolini",
+                "secchini",
+                "grissinini",
+                "patasplash",
+                "majopatas",
+                "crocchette al sesamo",
+                "crocchette alla pancetta",
+                "biscotti al miglio e avena"
+        ));
+    }
+
 }
